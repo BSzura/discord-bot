3b8303a0cfb27f04fb4417064c38d3788f51a71b
BSzura <117302856+BSzura@users.noreply.github.com>
2025-01-21 18:48:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
 import discord
 from discord.ext import commands
+import os
 from flask import Flask
 from threading import Thread
-import os
 
-# Intents
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
-
-# Bot
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-# Flask server
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot działa!"
+    return "Hello, I am alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
     t.start()
 
-# Server advertisement
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Reklama Twojego serwera
 server_ad = """
 **Jesteś doświadczonym programistą i szukasz forum, gdzie uzyskasz wsparcie i podzielisz się efektem swojej pracy? A może dopiero zaczynasz swoją przygodę z kodowaniem? Niezależnie od stopnia zaawansowania zapraszamy na nasz serwer programistyczny.**
 
@@ -45,10 +42,9 @@ Kogo szukamy:
 https://discord.gg/pPss9qWZ6p
 https://share.creavite.co/67646e7f0ae0e4f686a629f9.gif
 https://share.creavite.co/67646f950ae0e4f686a62a01.gif
-""";
+"""
 
-
-# Partnering users list
+# Lista użytkowników, którzy rozpoczęli proces partnerstwa
 partnering_users = {}
 
 @bot.event
@@ -60,38 +56,32 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    if "partner" in message.content.lower() and message.author.id not in partnering_users: 
-        partnering_users[message.author.id] = None 
-        await message.channel.send("🌎 Wyślij swoją reklamę (maksymalnie 1 serwer).")
-
+    if "partner" in message.content.lower() and message.author.id not in partnering_users:
+        partnering_users[message.author.id] = None
+        await message.channel.send("🌎 Witaj! Jeśli chcesz nawiązać partnerstwo, wyślij proszę swoją reklame (maksymalnie 1 serwer).")
+    
     elif message.author.id in partnering_users:
         if partnering_users[message.author.id] is None:
-            partnering_users[message.author.id] = message.content
-            await message.channel.send(f"✅ Wstaw naszą reklamę:\n{server_ad}")
-            await message.channel.send("⏰ Daj znać, gdy wstawisz reklamę!")
+            partnering_users[message.author.id] = message.content  # Przechowaj treść reklamy użytkownika
+            await message.channel.send(f"✅ Świetnie! Teraz wstaw naszą reklamę:\n{server_ad}")
+            await message.channel.send("⏰ Daj znać gdy wstawisz reklamę, a wtedy my wstawimy twoją!")
         elif "wstawi" in message.content.lower():
             guild = discord.utils.get(bot.guilds, id=1316466087570706432)
-            if not guild:
-                await message.channel.send("❕ Nie znaleziono serwera.")
-                return
-
-            if not discord.utils.get(guild.members, id=message.author.id):
-                await message.channel.send("❕ Dołącz na serwer, aby kontynuować!")
+            if guild and not discord.utils.get(guild.members, id=message.author.id):
+                await message.channel.send("❕ Zanim kontynuujemy, musisz dołączyć na serwer!")
             else:
-                channel = discord.utils.get(guild.text_channels, name="🤝partnerstwa")
-token = DISCORD_TOKEN
-if token:
-    client.login(token)
-else:
-    print("Token bota nie został ustawiony. Upewnij się, że zmienna środowiskowa DISCORD_TOKEN jest poprawnie skonfigurowana.")
+                channel = discord.utils.get(guild.text_channels, name="🤝partnerstwa")  # Nazwa kanału partnerstwa
+                if channel:
+                    user_ad = partnering_users[message.author.id]
+                    await channel.send(user_ad)  # Wstawienie reklamy użytkownika na kanał partnerstwa
+                    await message.channel.send("✅ Dziękujemy za nawiązanie partnerstwa!")
+                    partnering_users.pop(message.author.id)  # Usuń użytkownika z listy partnerstw po zakończeniu procesu
 
+    await bot.process_commands(message)
 
+keep_alive()
 
-
-
-
-
-
-
-
-
+try:
+    bot.run('MTMyNTgwNzI5MTAzOTA5Mjc1Ng.GZm_sa.SRJM7a0WIAHUceNEGVRjW1IRGHNlxfH-ZIf1RQ')
+except Exception as e:
+    print(f'Error: {e}')
